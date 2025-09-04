c950b2857f0867bdf0db59deaf7b4409b779f901
adityaaahirwar <adityahirwar21@gmail.com>
2025-09-04 22:35:26 +0530

    add

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://physiospot-backend-1.onrender.com/api/v1";
+export const BASE_URL = "https://physiospot.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
